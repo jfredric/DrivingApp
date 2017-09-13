0db27fd8d61c083120fb96eae24ecb9330896a5e
jfredrickson <jfredric@gmail.com>
2017-09-13 14:28:00 -0700

    initial commit

---
+++ DrivingApp/Car.swift
@@ -0,0 +1,13 @@
+//
+//  Car.swift
+//  DrivingApp
+//
+//  Created by Joshua Fredrickson on 9/13/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    
+}
---
+++ DrivingApp/DrivingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrivingApp
+//
+//  Created by Joshua Fredrickson on 9/13/17.
+//  Copyright © 2017 Joshua Fredrickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
